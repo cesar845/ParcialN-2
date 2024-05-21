b22b9570474a3b7cd59e907b00afe165c9e41c37
cesar845 <158522308+cesar845@users.noreply.github.com>
2024-05-21 16:33:22 -0500

    Add files via upload

---
+++ Activity.py
@@ -0,0 +1,9 @@
+import os
+
+def fnt_limpiar():
+    os.system('cls')
+    print('>>>>> SPORT SHOP <<<<<')
+    print('By: Cesar A. Castro P.')
+    
+def fnt_order():
+    print('>>>>> SPORT SHOP <<<<< \n.')
